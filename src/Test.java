d9d5f64bb1c60397daa93525af401c3e72157ff0
Moroianu Alexandru <moroianu.alexandru@gmail.com>
2021-08-04 15:38:59 +0300

    Added function which computes sum

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public int sum(int x, int y) {
+        return x+y;
+    }
 }
