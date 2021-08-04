9280dd4942d4aeefd4d02cb8e9560c57cd379c87
Moroianu Alexandru <moroianu.alexandru@gmail.com>
2021-08-04 15:51:11 +0300

    Add javadoc - Review requested

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,12 @@ public class Test {
         System.out.println("Hello world!");
     }
 
+    /**
+     * Computes the addition of two numbers.
+     * @param x - number one
+     * @param y - number two
+     * @return - sum of them.
+     */
     public int sum(int x, int y) {
         return x+y;
     }
